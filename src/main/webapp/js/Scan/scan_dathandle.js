300d869d2aae91a546295adfd984fff725294646
LZR <LZR@DESKTOP-3Q2DMA4>
2018-12-03 23:14:52 +0800

    LZR

--- src/main/webapp/js/Scan/scan_dathandle.js
+++ src/main/webapp/js/Scan/scan_dathandle.js
@@ -1,37 +1,56 @@
 ﻿function handleData(d)
 {
-	
-    alert(d);
 	var d = JSON.parse(d)
     var cdkey = d[0];//获取CD-KEY
+    var platxt = d[1];
     var secert=d[2];//获取密文
-    var data = {"cdk": d[0], "info": md5(d[1])};
-    $.ajax({
-        type: "POST",
-        url: "search",        
-        data: data,      
-        success: function (data, textStatus, jqXHR) {
-        	var json = JSON.parse(data);
-        	var private_key = json.public_key;//获取私钥
-        	alert(private_key);
-        	 
-        	 // Decrypt with the private key...
-            var decrypt1 = new JSEncrypt();
-            decrypt1.setPrivateKey(private_key);
-            var uncrypted = decrypt1.decrypt(secert);
-            alert(uncrypted);
-            // Now a simple check to see if the round-trip worked.
-            if (uncrypted == cdkey) {
-                alert("验证成功！");
-            }
-            else {
-                alert("验证失败！");
-            }
-        
-        },
-        error: function () {
-           alert("上传数据失败！");
-        }
-        
+    $("#dialog1").addClass("show");
+    $("#cdkey").append(cdkey);
+    $("#information").append(platxt);
+    $("#back").click(function(){
+        $("#dialog1").removeClass("show");
+        location.reload(false);
     });
+    $("#ver").click(function(){
+        var data = {"cdk": d[0], "info": md5(d[1])};
+        $.ajax({
+            type: "POST",
+            url: "search",
+            data: data,
+            success: function (data, textStatus, jqXHR) {
+                var json = JSON.parse(data);
+                var private_key = json.public_key;//获取私钥
+                 // Decrypt with the private key...
+                var decrypt = new JSEncrypt();
+                decrypt.setPrivateKey(private_key);
+                var uncrypted = decrypt.decrypt(secert);
+                // Now a simple check to see if the round-trip worked.
+                if (uncrypted == cdkey) {
+                    $("#dialog2").addClass("show");
+                    $("#result").text("Successful Authentication!");
+                    $("#ok").click(function(){
+                        location.reload(false);
+                    });
+                }
+                else {
+                    $("#dialog2").addClass("show");
+                    $("#result").text("Failed Authentication!");
+                    $("#ok").click(function(){
+                        location.reload(false);
+                    });
+                }
+
+            },
+            error: function () {
+               $("#dialog2").addClass("show");
+               $("#result").text("Failed Transmission");
+               $("#ok").click(function(){
+                        location.reload(false);
+                    });
+            }
+
+        });
+
+    });
+
 }
